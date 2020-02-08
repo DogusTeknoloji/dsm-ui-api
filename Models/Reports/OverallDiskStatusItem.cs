13f35072dff2552ae41e1d438625f6c768098982
Onur Akkaya <onur.akkaya@d-teknoloji.com.tr>
2020-02-08 22:26:01 +0300

    Updated OverallDiskstatus-Report -> Volume Field added.

--- Models/Reports/OverallDiskStatusItem.cs
+++ Models/Reports/OverallDiskStatusItem.cs
@@ -6,6 +6,7 @@ namespace DSM.UI.Api.Models.Reports
         public string CompanyName { get; set; }
         public string ServerName { get; set; }
         public DateTime? LastCheckDate { get; set; }
+        public string VolumeName { get; set; }
         public string DiskCapacity { get; set; }
         public string UsedDiskSpace { get; set; }
         public string FreeDiskSpace { get; set; }
