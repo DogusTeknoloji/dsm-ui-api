6b286983fc9929fbdee7c9978c97f712c02a51a1
Furkan Evren GUZEL <furkanevrenguzel@hotmail.com>
2021-09-03 17:53:09 +0300

    Creating kpi status metric views

---
+++ Models/Reports/KPIMetricsView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSM.UI.Api.Models.Reports
+{
+    public class KPIMetricsView
+    {
+        public string Application { get; set; }
+        public int Year { get; set; }
+        public string Ocak { get; set; }
+        public string Subat { get; set; }
+        public string Mart { get; set; }
+        public string Nisan { get; set; }
+        public string Mayis { get; set; }
+        public string Haziran { get; set; }
+        public string Temmuz { get; set; }
+        public string Agustos { get; set; }
+        public string Eylul { get; set; }
+        public string Ekim { get; set; }
+        public string Kasim { get; set; }
+        public string Aralik { get; set; }
+    }
+}
