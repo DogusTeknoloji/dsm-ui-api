2400f2f91d2c80c34fcb517d45489e9bf4b788e1
selim <53125635+selimaytac@users.noreply.github.com>
2022-10-19 09:52:33 +0300

    Another customer's server list added.

--- Services/InventoryTrackingService.cs
+++ Services/InventoryTrackingService.cs
@@ -10,14 +10,40 @@ namespace DSM.UI.Api.Services
 {
     public interface IInventoryTrackingService
     {
-        Task<IEnumerable<UpdatedSiteInventoryItem>> GetAllSiteInventory();
-        Task<UpdatedSiteInventoryItem> GetSiteInventoryItem(int id);
-        Task<UpdatedSiteInventoryItem> AddUpdatedSiteInventoryItemAsync(UpdatedSiteInventoryItem UpdatedSiteInventoryItem);
-        Task<UpdatedSiteInventoryItem> UpdateUpdatedSiteInventoryItemAsync(UpdatedSiteInventoryItem UpdatedSiteInventoryItem);
+        #region Updated Site Inventory Tracking
+
+        Task<IEnumerable<UpdatedSiteInventoryItem>> GetAllSiteInventoryAsync();
+        Task<UpdatedSiteInventoryItem> GetSiteInventoryItemAsync(int id);
+
+        Task<UpdatedSiteInventoryItem> AddUpdatedSiteInventoryItemAsync(
+            UpdatedSiteInventoryItem UpdatedSiteInventoryItem);
+
+        Task<UpdatedSiteInventoryItem> UpdateUpdatedSiteInventoryItemAsync(
+            UpdatedSiteInventoryItem UpdatedSiteInventoryItem);
+
         Task<bool> DeleteUpdatedSiteInventoryItemAsync(int id);
         byte[] DownloadUpdatedSiteInventory();
         byte[] DownloadUpdatedSiteInventory(object term);
+
+        #endregion
+        
+        # region DetailedServerListR 
+        Task<IEnumerable<DetailedServerInventoryItem>> GetAllDetailedServerInventoryAsync();
+        Task<DetailedServerInventoryItem> GetDetailedServerInventoryItemAsync(int id);
+
+        Task<DetailedServerInventoryItem> AddDetailedServerInventoryAsync(
+            DetailedServerInventoryItem detailedServerInventoryItem);
+
+        Task<DetailedServerInventoryItem> UpdateDetailedServerInventoryAsync(
+            DetailedServerInventoryItem detailedServerInventoryItem);
+
+        Task<bool> DeleteDetailedServerInventoryAsync(int id);
+        byte[] DownloadDetailedServerInventory();
+        byte[] DownloadDetailedServerInventory(object term);
+
+        # endregion
     }
+
     public class InventoryTrackingService : IInventoryTrackingService
     {
         private readonly DSMStorageDataContext _context;
@@ -27,25 +53,27 @@ namespace DSM.UI.Api.Services
             _context = context;
         }
 
-        
-        public async Task<IEnumerable<UpdatedSiteInventoryItem>> GetAllSiteInventory()
+
+        public async Task<IEnumerable<UpdatedSiteInventoryItem>> GetAllSiteInventoryAsync()
         {
             return await _context.UpdatedSiteInventoryItems.AsNoTracking().ToListAsync();
         }
 
-        public async Task<UpdatedSiteInventoryItem> GetSiteInventoryItem(int id)
+        public async Task<UpdatedSiteInventoryItem> GetSiteInventoryItemAsync(int id)
         {
             return await _context.UpdatedSiteInventoryItems.FindAsync(id);
         }
 
-        public async Task<UpdatedSiteInventoryItem> AddUpdatedSiteInventoryItemAsync(UpdatedSiteInventoryItem UpdatedSiteInventoryItem)
+        public async Task<UpdatedSiteInventoryItem> AddUpdatedSiteInventoryItemAsync(
+            UpdatedSiteInventoryItem UpdatedSiteInventoryItem)
         {
             _context.UpdatedSiteInventoryItems.Add(UpdatedSiteInventoryItem);
             await _context.SaveChangesAsync();
             return UpdatedSiteInventoryItem;
         }
 
-        public async Task<UpdatedSiteInventoryItem> UpdateUpdatedSiteInventoryItemAsync(UpdatedSiteInventoryItem UpdatedSiteInventoryItem)
+        public async Task<UpdatedSiteInventoryItem> UpdateUpdatedSiteInventoryItemAsync(
+            UpdatedSiteInventoryItem UpdatedSiteInventoryItem)
         {
             if (!_context.UpdatedSiteInventoryItems.Any(x => x.Id == UpdatedSiteInventoryItem.Id))
                 return null;
@@ -107,6 +135,94 @@ namespace DSM.UI.Api.Services
                         Company = x.Company,
                         Responsible = x.Responsible,
                         Owner = x.Owner
+                    }
+                );
+            }
+
+            return ExcelOperations.ExportToExcel(results);
+        }
+
+        public async Task<IEnumerable<DetailedServerInventoryItem>> GetAllDetailedServerInventoryAsync()
+        {
+            return await _context.DetailedServerInventoryItems.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<DetailedServerInventoryItem> GetDetailedServerInventoryItemAsync(int id)
+        {
+            return await _context.DetailedServerInventoryItems.FindAsync(id);
+        }
+
+        public async Task<DetailedServerInventoryItem> AddDetailedServerInventoryAsync(DetailedServerInventoryItem detailedServerInventoryItem)
+        {
+            _context.DetailedServerInventoryItems.Add(detailedServerInventoryItem);
+            await _context.SaveChangesAsync();
+            return detailedServerInventoryItem;
+        }
+
+        public async Task<DetailedServerInventoryItem> UpdateDetailedServerInventoryAsync(DetailedServerInventoryItem detailedServerInventoryItem)
+        {
+            if (!_context.DetailedServerInventoryItems.Any(x => x.Id == detailedServerInventoryItem.Id))
+                return null;
+
+            _context.DetailedServerInventoryItems.Update(detailedServerInventoryItem);
+            await _context.SaveChangesAsync();
+            return detailedServerInventoryItem;
+        }
+
+        public async Task<bool> DeleteDetailedServerInventoryAsync(int id)
+        {
+            var deletedDetailedSiteInventoryItem = await _context.DetailedServerInventoryItems.FindAsync(id);
+
+            if (deletedDetailedSiteInventoryItem == null)
+                return false;
+
+            _context.DetailedServerInventoryItems.Remove(deletedDetailedSiteInventoryItem);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public byte[] DownloadDetailedServerInventory()
+        {
+            return DownloadDetailedServerInventory(null);
+        }
+
+        public byte[] DownloadDetailedServerInventory(object term)
+        {
+            IEnumerable<DetailedServerInventoryItem> results = null;
+            if (term == null)
+            {
+                var query = _context.DetailedServerInventoryItems.AsQueryable();
+                results = query.ToList().Select(
+                    x => new DetailedServerInventoryItem
+                    {
+                        Id = x.Id,
+                        ServerName = x.ServerName,
+                        OS = x.OS,
+                        IP = x.IP,
+                        Responsible = x.Responsible,
+                        Server = x.Server,
+                        Application = x.Application
+                    }
+                );
+            }
+            else
+            {
+                var query = _context.DetailedServerInventoryItems.AsQueryable();
+                var stringProperties = typeof(DetailedServerInventoryItem).GetProperties()
+                    .Where(prop => prop.PropertyType == term.GetType());
+
+                query = EntityQueryable.WhereContains(query, stringProperties, term.ToString());
+
+                results = query.ToList().Select(
+                    x => new DetailedServerInventoryItem
+                    {
+                        Id = x.Id,
+                        ServerName = x.ServerName,
+                        OS = x.OS,
+                        IP = x.IP,
+                        Responsible = x.Responsible,
+                        Server = x.Server,
+                        Application = x.Application
                     }
                 );
             }
